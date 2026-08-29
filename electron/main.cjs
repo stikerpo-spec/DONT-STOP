98b0ea50912c5772573b1f2096b5b26d5914e370
stikerpo-spec <stikerpo@gmail.com>
2026-08-29 22:01:31 +0200

    Add real Windows desktop app entry point

---
+++ electron/main.cjs
@@ -0,0 +1,33 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('node:path');
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 520,
+    height: 900,
+    minWidth: 420,
+    minHeight: 650,
+    backgroundColor: '#050711',
+    title: "DON'T STOP",
+    autoHideMenuBar: true,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+      sandbox: true
+    }
+  });
+
+  Menu.setApplicationMenu(null);
+  win.loadFile(path.join(__dirname, '..', 'game.html'));
+};
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
