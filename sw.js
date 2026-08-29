58b4ea933ed570a1c0fdfbbed4e7b422fc8c093f
stikerpo-spec <stikerpo@gmail.com>
2026-08-29 21:55:08 +0200

    Fix offline cache and include game files

--- sw.js
+++ sw.js
@@ -1,23 +1,42 @@
-const CACHE = 'dont-stop-v1';
-const ASSETS = ['./', './index.html', './save.js', './manifest.webmanifest', './icon.svg'];
+const CACHE = 'dont-stop-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './game.html',
+  './game.js',
+  './save.js',
+  './manifest.webmanifest',
+  './icon.svg'
+];
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
+
   event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
-      const copy = response.clone();
-      caches.open(CACHE).then(cache => cache.put(event.request, copy));
-      return response;
-    }).catch(() => caches.match('./index.html')))
+    caches.match(event.request).then(cached => {
+      if (cached) return cached;
+      return fetch(event.request).then(response => {
+        if (!response || !response.ok || response.type === 'opaque') return response;
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy)).catch(() => {});
+        return response;
+      }).catch(() => caches.match('./index.html'));
+    })
   );
 });
