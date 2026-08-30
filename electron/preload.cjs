3eb096fffcca0bb0706957c56d9952ac89583c40
stikerpo-spec <stikerpo@gmail.com>
2026-08-30 11:16:35 +0200

    Expose safe Discord presence IPC to renderer

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -3,3 +3,8 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('DontStopElectronUpdater', {
   downloadAndInstall: (url) => ipcRenderer.invoke('dont-stop:download-and-install', url)
 });
+
+contextBridge.exposeInMainWorld('DontStopDiscord', {
+  update: (payload) => ipcRenderer.invoke('dont-stop:discord-update', payload),
+  clear: () => ipcRenderer.invoke('dont-stop:discord-clear')
+});
