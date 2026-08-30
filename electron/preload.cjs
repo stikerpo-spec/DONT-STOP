f040594ef909ba3df14e47af2577bceaceb2e8fc
stikerpo-spec <stikerpo@gmail.com>
2026-08-30 12:31:09 +0200

    Remove Discord bridge from desktop app

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -3,14 +3,3 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('DontStopElectronUpdater', {
   downloadAndInstall: (url) => ipcRenderer.invoke('dont-stop:download-and-install', url)
 });
-
-contextBridge.exposeInMainWorld('DontStopElectronDiscord', {
-  setActivity: (payload) => ipcRenderer.invoke('dont-stop:discord-update', payload),
-  clearActivity: () => ipcRenderer.invoke('dont-stop:discord-clear')
-});
-
-// Backward-compatible alias for older builds.
-contextBridge.exposeInMainWorld('DontStopDiscord', {
-  update: (payload) => ipcRenderer.invoke('dont-stop:discord-update', payload),
-  clear: () => ipcRenderer.invoke('dont-stop:discord-clear')
-});
